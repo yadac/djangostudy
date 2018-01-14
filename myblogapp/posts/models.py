b0a7cf69559335509c60c004711d12a6cd1a02bf
yosuke adachi <doruji1982@gmail.com>
2018-01-14 15:10:57 +0900

    3-26 template engine

--- myblogapp/posts/models.py
+++ myblogapp/posts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    publishd = models.DateTimeField()
+    published = models.DateTimeField()
     image = models.ImageField(upload_to='media/')
     body = models.TextField()
 
--- myblogapp/posts/views.py
+++ myblogapp/posts/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 def index(request):
     # return HttpResponse("Hello World! This page is posts index.")
+    # get all objects from Posts
+    posts = Post.objects.order_by('-published')
     # using templates
-    return render(request, 'posts/index.html')
+    # args = posts
+    return render(request, 'posts/index.html', {'posts': posts})
