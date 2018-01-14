dba537f440b75db82bc73a75f7621a79f0c2edb9
yosuke adachi <doruji1982@gmail.com>
2018-01-14 14:09:05 +0900

    3-22 templates

--- myblogapp/posts/views.py
+++ myblogapp/posts/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello World! This page is posts index.")
+    # return HttpResponse("Hello World! This page is posts index.")
+    # using templates
+    return render(request, 'posts/index.html')
