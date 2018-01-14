02a867dd44688422ac1c2f8cb101bdce9b86c1be
yosuke adachi <doruji1982@gmail.com>
2018-01-14 14:59:20 +0900

    3-25 admin with model

--- myblogapp/posts/models.py
+++ myblogapp/posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    publishd = models.DateTimeField()
+    image = models.ImageField(upload_to='media/')
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
