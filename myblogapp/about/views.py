51e55ec69d0d53fe7b783c42dead250b1ee54ac2
yosuke adachi <doruji1982@gmail.com>
2018-01-16 23:05:20 +0900

    models, bootstrap

--- myblogapp/about/views.py
+++ myblogapp/about/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Author
 
 def index(request):
-    return render(request, 'about/index.html')
+    us = get_object_or_404(Author, pk=1)
+    return render(request, 'about/index.html', {'us': us})
